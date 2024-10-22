9d5e56894013edc8c144f8f0a7fa159d2d86bde4
Nabarka1999 <82209383+Nabarka1999@users.noreply.github.com>
2024-10-22 17:34:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ import cors from 'cors';
 dotenv.config();
 
 export const instance = new Razorpay({
-  key_id: process.env.Razorpay_key,
-  key_secret: process.env.Razorpay_secret,
+  key_id: process.env.RAZORPAY_KEY,
+  key_secret: process.env.RAZORPAY_SECRET,
 });
 
 const app = express();
